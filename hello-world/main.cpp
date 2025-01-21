55407f37360715f60f983fcb9b021147f2498701
Kaden Duckwitz <kadenduckwitz@westernwyoming.edu>
2025-01-21 04:26:31 +0000

    Hello world assignment - test commit and push.

--- hello-world/main.cpp
+++ hello-world/main.cpp
@@ -1,7 +1,14 @@
 //
-// Name
-// Date
+// Kaden Duckwitz
+// 1/20/2025
 // Hello World Programming Project
 // COSC 1030
 //
 
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, World!";
+    return 0;
+}
