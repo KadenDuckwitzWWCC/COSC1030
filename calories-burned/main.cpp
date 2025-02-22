bab8dd823d8d952511317191edb18f574730b057
Kaden Duckwitz <kadenduckwitz@westernwyoming.edu>
2025-02-22 04:49:26 +0000

    Calories Burned Programming Project

--- calories-burned/main.cpp
+++ calories-burned/main.cpp
@@ -1,7 +1,19 @@
 //
-// Name
-// Date
+// Kaden Duckwitz
+// 2/21/2025
 // Calories Burned Programming Project
 // COSC 1030
 //
 
+#include <iostream>
+
+const double CALORIES_PER_MIN = 3.6;
+
+int main() {
+    for (int minutes = 5; minutes <= 30; minutes += 5) {
+        double caloriesBurned = minutes * CALORIES_PER_MIN;
+
+        std::cout << "After " << minutes << " minutes, you burned ";
+        std::cout << caloriesBurned << " calories." << std::endl;
+    }
+}
