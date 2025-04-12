b459fcbad3042158c9238ddf43659fc58f2c44d1
Kaden Duckwitz <kadenduckwitz@westernwyoming.edu>
2025-04-12 04:58:21 +0000

    Pointer Rewrite Programming Project

--- pointer-rewrite/main.cpp
+++ pointer-rewrite/main.cpp
@@ -1,7 +1,36 @@
 //
-// Name
-// Date
+// Kaden Duckwitz
+// 4/11/25
 // Pointer Rewrite Programming Project
 // COSC 1030
 //
 
+#include <iostream>
+
+int doSomething(int *, int *);
+
+int main()
+{
+    int x = 10, y = 2;
+
+    std::cout << "Before calling the function:\n" 
+    << "x = " << x << std::endl 
+    << "y = " << y << std::endl << std::endl;
+
+    std::cout << "The function returns " 
+    << doSomething(&x, &y) << std::endl << std::endl;
+
+    std::cout << "After calling the function:\n" 
+    << "x = " << x << std::endl 
+    << "y = " << y << std::endl << std::endl;
+
+    return 0;
+}
+
+int doSomething(int *x, int *y)
+{
+    int temp = *x;
+    *x = *y * 10;
+    *y = temp * 10;
+    return *x + *y;
+}
