1aed5606f369d58b92658ca93f0b50849b14e785
Kaden Duckwitz <kadenduckwitz@westernwyoming.edu>
2025-04-19 16:39:52 +0000

    Backward String Programming Project

--- backward-string/main.cpp
+++ backward-string/main.cpp
@@ -1,7 +1,34 @@
 //
-// Name
-// Date
+// Kaden Duckwitz
+// 4/19/25
 // Backward String Programming Project
 // COSC 1030
 //
 
+#include <iostream>
+#include <cstring>
+
+void displayBackwardString(char *);
+
+int main()
+{
+    const int SIZE = 80;
+    char input[SIZE];
+
+    std::cout << "Enter a string and I will display it backward:\n";
+    std::cin.getline(input, SIZE);
+
+    displayBackwardString(input);
+
+    return 0;
+}
+
+void displayBackwardString(char *str)
+{
+    for (int i = strlen(str) - 1; i >= 0; i--)
+    {
+        std::cout << str[i];
+    }
+
+    std::cout << std::endl;
+}
