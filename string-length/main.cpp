b14bf02d649c7350c636b86795d9fc2be685a76c
Kaden Duckwitz <kadenduckwitz@westernwyoming.edu>
2025-04-26 06:02:09 +0000

    String Length Programming Project

--- string-length/main.cpp
+++ string-length/main.cpp
@@ -1,7 +1,36 @@
 //
-// Name
-// Date
+// Kaden Duckwitz
+// 4/25/25
 // String Length Programming Project
 // COSC 1030
 //
 
+#include <iostream>
+
+const int MAX_SIZE = 100;
+
+int stringLength(char []);
+
+int main()
+{
+    char inputString[MAX_SIZE];
+
+    std::cout << "Please input a string: ";
+    std::cin.getline(inputString, MAX_SIZE);
+
+    int length = stringLength(inputString);
+
+    std::cout << "The length of the string is: " << length << std::endl;
+}
+
+int stringLength(char string[])
+{
+    int index = 0;
+
+    while (string[index] != '\0')
+    {
+        index++;
+    }
+
+    return index;
+}
